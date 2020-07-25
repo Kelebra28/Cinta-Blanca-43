58d34e674737a086994e850d9724b205ba44fed5
Kelebra <ricardokelebra@gmail.com>
2020-07-24 19:47:18 -0500

    Intro a JavaScript

---
+++ 04_IntroJS/main.js
@@ -0,0 +1,19 @@
+// alert('Descarga!!!!!!!!!!!!!!!!')
+
+// var nombre = valor
+
+//------ varlor numerico
+var numero = 50
+// ----- valor texto o string
+var nombre = 'Ricardo'
+// ------- valor booeleano o boolean
+var verdadero = true // -> false
+
+// ------- variables especiales-----
+
+// --array o arreglo
+var arreglo = []
+//--- objeto o object
+var objeto = {}
+
+
