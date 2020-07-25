3ff36b824f942c53a6a9af794ef22eb5fa22a658
Kelebra <ricardokelebra@gmail.com>
2020-07-24 21:44:28 -0500

    Intro a JavaScript

--- 04_IntroJS/main.js
+++ 04_IntroJS/main.js
@@ -1,5 +1,10 @@
 // alert('Descarga!!!!!!!!!!!!!!!!')
 
+
+
+//------------------------------------Vanilla JS------------------------------
+
+
 // var nombre = valor
 
 //------ varlor numerico
@@ -26,19 +31,56 @@ var num2 = 500
 console.log(num1 + num2)
 
 var resultado = num1 + num2
-
-console.log(resultado)
-console.log(nombre)
+// console.log(resultado)
+// console.log(nombre)
 
 
 var saludo = 'Hola'
 var sensei = 'kelebra'
 
-console.log(saludo + ' ' + sensei)
+// console.log(saludo + ' ' + sensei)
 
 
 // --------------------- entradas o inputs--------
 //             prompt me permite  que el usuario ingrese datos                    
-var pregunta = prompt('Ingresa tu nombre')
+// var pregunta = prompt('Ingresa tu nombre')
 
-console.log(pregunta)
+// console.log(pregunta)
+
+
+// --------------------------Challenge--------------------------
+
+// -> Preguntar al usuario su nombre direccion y telefono
+//    mostrar cada resultado en la consola
+// -> Pregutar al usuario su edad cumplira este año y mostrar en la consola:
+//    que año nacio
+
+
+//-------Paso 1-----------
+// var preguntaNombre = prompt('Ingresa tu nombre')
+// var preguntaTelefono = prompt('Ingresa tu telefono')
+// var preguntaDireccion = prompt('Ingresa tu direccion')
+var preguntaEdad = prompt('Ingresa tu edad')
+
+// console.log(preguntaNombre)
+// console.log(preguntaTelefono)
+// console.log(preguntaDireccion)
+console.log(preguntaEdad) // --> '22'
+
+// ---------- Paso 2---------
+
+var anio = 2020
+
+// typeof() -> me indica que tipo de valor es 
+console.log(typeof(preguntaEdad)) // ---> mostar que tipo de valor es preguntaEdad
+
+// Number()  ----> Pasa el tipo de valor a numerico
+
+var edadReal = Number(preguntaEdad) // '22' -> 22
+
+console.log(edadReal)
+console.log(typeof(edadReal))
+
+var nacimiento = anio - edadReal
+
+console.log(nacimiento)
