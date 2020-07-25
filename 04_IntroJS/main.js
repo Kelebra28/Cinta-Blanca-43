0a9ff0158c535e7afe7e6a0da7629d89a47aba64
Kelebra <ricardokelebra@gmail.com>
2020-07-24 20:19:09 -0500

    Intro a JavaScript

--- 04_IntroJS/main.js
+++ 04_IntroJS/main.js
@@ -12,8 +12,26 @@ var verdadero = true // -> false
 // ------- variables especiales-----
 
 // --array o arreglo
-var arreglo = []
+var arreglo = [] //---> corchetes
 //--- objeto o object
-var objeto = {}
+var objeto = {} // ---> llaves
 
 
+////-------------------------------------
+
+var num1 = 100
+var num2 = 500
+// salidas o outputs
+
+console.log(num1 + num2)
+
+var resultado = num1 + num2
+
+console.log(resultado)
+console.log(nombre)
+
+
+var saludo = 'Hola'
+var sensei = 'kelebra'
+
+console.log(saludo + ' ' + sensei)
