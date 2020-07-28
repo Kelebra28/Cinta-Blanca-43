d3b61e1f44c55defd711088f71d923618e74f19e
Kelebra <ricardokelebra@gmail.com>
2020-07-27 19:32:09 -0500

    Condicionales

---
+++ 05_Condionales/main.js
@@ -0,0 +1,28 @@
+// alert('Hola Mundo')
+
+// ---------------> Condicionales <------------
+
+// if (condicion) {
+//     pasos a seguir si la condicion es verdadera
+// } else {
+//     pasos a seguir si la condicion es falsa
+// }
+
+// ------> Operadores Relacionales
+// menor que : <
+// mayor que : >
+// menor igual : <=
+// mayor igual : >=
+//----------------
+// comparar:  ==
+// comparar identicamente : ===
+// diferente de : !==
+
+
+var num = 10
+
+if(num <= 10){
+    console.log('Es verdadero')
+}else{
+    console.log('Es falso')
+}
