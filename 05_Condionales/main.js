600a82974ed720b5ab10cab7beea968806238081
Kelebra <ricardokelebra@gmail.com>
2020-07-27 20:03:33 -0500

    Condicionales

--- 05_Condionales/main.js
+++ 05_Condionales/main.js
@@ -14,15 +14,39 @@
 // menor igual : <=
 // mayor igual : >=
 //----------------
-// comparar:  ==
+// comparar:  == -> Por buenas practicas ni lo usen :)
 // comparar identicamente : ===
 // diferente de : !==
 
 
-var num = 10
+var num = 'Hola'
 
-if(num <= 10){
+if(num === 10){
     console.log('Es verdadero')
 }else{
     console.log('Es falso')
 }
+
+
+// -------------------------- Challenge --------------------
+// password: perritoSalchicha123
+
+// Le van a preguntar al usuaria si contraseña.
+// Si es correcta, en la consola dira: 'Bienvenido'.
+// Si es incorrecta, en la consola dira : 'Intentalo de nuevo'.
+
+// var pregunta = prompt('Ingresa tu contraseña')
+
+// var password = 'perritoSalchicha123'
+
+// if(pregunta === password) {
+//     console.log('Bienvenido')
+// }else {
+//     console.log('Intentalo de nuevo')
+// }
+
+// ------------------ 2 Challenge --------
+
+// Le van a preguntar al usuario que les de un numero.
+// Van a decir si este numero es par o es impar.
+// modulo -> % <-
