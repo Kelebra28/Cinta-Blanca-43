41c938bad4d797364ae766ce99d6e335542f5846
Kelebra <ricardokelebra@gmail.com>
2020-07-28 19:58:23 -0500

    Arrreglos y Objetos

--- 06_Arreglos-Obejtos/main.js
+++ 06_Arreglos-Obejtos/main.js
@@ -26,3 +26,21 @@ colores.pop()
 console.log(colores)
 
 
+//---------- .splice()
+// Eliminar valores espesificos o sustituirlos
+// -> Primer valor: en que posicion queiren empezar
+// -> Segundo valor: cuantos valores quieres eliminar
+// -> Tercer valor: Elemento que quieres agregar
+colores.splice(1, 1, 'Prueba')
+console.log(colores)
+
+//--------> Challenge <-------
+// Hacer un arreglo de musica vacio, agregar 5 generos con el metodo push,
+// remplazar la posicion numero 3 con "PerritoSalchicha" y por ultimo borrar el ultimo elemento con el metodo pop. 
+
+var musica = []
+
+musica.push('Reggeton', 'Trap', 'Clasica', 'Salsa', 'Cumbia')
+musica.splice(3, 1 , 'PerritoSalchicha')
+musica.pop()
+console.log(musica)
