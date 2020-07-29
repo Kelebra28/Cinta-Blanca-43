9c2a07f567fa03f7ea6921ff37b4a503298ea023
Kelebra <ricardokelebra@gmail.com>
2020-07-28 19:32:00 -0500

    Arrreglos y Objetos

---
+++ 06_Arreglos-Obejtos/main.js
@@ -0,0 +1,28 @@
+// -------------> Arreglo o Array <------------
+
+// Pocisiones:   0        1        2       3
+var colores = ['Negro', 'Rojo', 'Azul', 'Verde']
+
+console.log(colores)
+// -> Acceder a a un valor  
+console.log(colores[2])
+
+
+// colores[0] = 'Blanco'
+// console.log(colores)
+
+// ------- .push()
+// Agrega un elemento en la ultima posicon
+
+colores.push('Blanco') // -> 4
+colores.push('Naranja') // -> 5
+
+console.log(colores)
+
+// ---------- .pop()
+// Elimina el ultimo valor del arreglo
+
+colores.pop()
+console.log(colores)
+
+
