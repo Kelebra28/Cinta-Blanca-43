5f044b18bb86ff65f5bfaee2a9b3081236947f7f
Kelebra <ricardokelebra@gmail.com>
2020-08-04 19:50:27 -0500

    Document Object Model

--- 11_DOM/main.js
+++ 11_DOM/main.js
@@ -1,11 +1,20 @@
-let titutlo = document.getElementById('titulo')
-let subtitulo = document.getElementById('subtitulo')
-let btn = document.getElementById('btn')
+// document.getElementById -> busca en todo el documento HTML un ID
+let titutlo = document.getElementById('titulo') // -> h1
+let subtitulo = document.getElementById('subtitulo') // -> h2
+let input = document.getElementById('pregunta') // -> input
+let texto = document.getElementById('respuesta') // -> p
+let btn = document.getElementById('btn') // -> button
 
 const cambio = () => {
-
+    
+    //innterHTML -> Mostar o pintar en la etiqueta un valor
     titutlo.innerHTML = 'Soy el titulo cambiado desde js 👹'
     subtitulo.innerHTML = 'Soy el subtitilo midificado desde js ⛓'
+    // -> .value: es el texto que ingreso en el input        
+    texto.innerHTML = input.value
+    console.log(input)
+    console.log(input.value)
+
 }
 
 btn.addEventListener('click', cambio)
