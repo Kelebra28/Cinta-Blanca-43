c915de579af34c7b330072279e3d70faf62eafee
Kelebra <ricardokelebra@gmail.com>
2020-08-05 21:57:16 -0500

    Chalenge Pizza

--- 11_Challenge/main.js
+++ 11_Challenge/main.js
@@ -12,7 +12,8 @@ let respuestaTelefono = document.getElementById('respuestaTelefono')
 let respuestaPizza = document.getElementById('respuestaPizza')
 // card
 let card = document.getElementById('card')
-
+// img
+let imgPizza = document.getElementById('imgPizza')
 
 const ordenar = () => {
 
@@ -22,14 +23,18 @@ const ordenar = () => {
  
     let pizzaValue = preguntaPizza.value.toLowerCase()
     console.log(pizzaValue)
+
     if(pizzaValue === 'no'){
         respuestaPizza.innerHTML = '🥰 Sin piña 🥰'
+        imgPizza.src = 'https://imagenes.milenio.com/CU287W9FW_sUIP1hV9nKcYoMLd0=/958x596/https://www.milenio.com/uploads/media/2019/06/03/no-hay-quien-se-resista.jpg'
+
     }else if(pizzaValue === 'si'){
         respuestaPizza.innerHTML = '🤬 Con piña 🤬'
+        imgPizza.src = 'https://t1.rg.ltmcdn.com/es/images/0/2/6/img_pizza_hawaiana_de_jamon_y_pina_50620_600.jpg'
     }else{
         respuestaPizza.innerHTML = '🚫 Datos incorrectos 🚫'
+        imgPizza.src = 'https://i.pinimg.com/originals/86/f2/34/86f2341d76ba2fbec6fc722c888b66a0.jpg'
     }
-
     //  console.log(card.classList)
     card.classList.remove('d-none')
     // console.log(card.classList)
@@ -41,3 +46,6 @@ btn.addEventListener('click', ordenar)
 // Si el usuario dice que "SI" -> :(
 // Si el usuario dice que "NO" -> :)    
 // Maquetar el formulario para que parecezca de una pagina real 
+
+// Si el usuari dice si -> imagen de pizza con piña
+// Si el usuario dice no -> imagen de pizza peperono
